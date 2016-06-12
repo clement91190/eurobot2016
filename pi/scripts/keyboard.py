75e375e9c13661f6c1085e1bcfca07d1c89f82a1
clement <clem.jambou@gmail.com>
2016-06-11 20:01:20 -0700

    better control
    g

--- pi/scripts/keyboard.py
+++ pi/scripts/keyboard.py
@@ -1,10 +1,11 @@
 import termios, fcntl, sys, os
-import serial 
+import time
+import serial
 
 
-def main(): 
+def main():
     fd = sys.stdin.fileno()
-    serr = serial.Serial('/dev/ttyUSB7', 9600, timeout=0.1)
+    serr = serial.Serial('/dev/ttyUSB0', 9600, timeout=0.1)
     serr.close()
     serr.open()
     print serr.readline()
@@ -18,31 +19,56 @@ def main():
     oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
     fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
     cap = 0
-
+    print  "start"
+    c = " "
     try:
         while 1:
             try:
                 c = sys.stdin.read(1)
-                if c == "A":
-                    print "av"
-                    serr.write('1550\n')
-                if c == "B":
-                    print"rec"
-                    serr.write('1450\n')
-                if c == "D":
-                    print "gauche"
-                    cap += 5
-                    serr.write('2530\n')
-                if c == "C":
-                    print "droite"
-                    cap -= 5
-                    serr.write('2470\n')
-                if c == "d":
-                    serr.write('D\n')
-                if c not in ["A", "B", "C", "D"]:
-                    print c, "not recognized -> STOP"
-                    serr.write('1500\n')
-            except IOError: pass
+            except IOError:
+                pass
+            sys.stdin.flush()
+
+            print c
+            if c == "A":
+                print "av"
+                serr.write('1530 ')
+                send_data = True
+            if c == "B":
+                print"rec"
+                serr.write('1470 ')
+                send_data = True
+            if c == "D":
+                print "gauche"
+                cap += 5
+                serr.write('2530 ')
+                send_data = True
+            if c == "C":
+                print "droite"
+                cap -= 5
+                serr.write('2470 ')
+                send_data = True
+            if c == " ":
+                serr.write('1500\n')
+                send_data = True
+            if c not in ["A", "B", "C", "D"]:
+                #print c, "not recognized -> STOP"
+                #serr.write('1500 ')
+                pass
+            if send_data:
+                try:
+                    data = sys.stdin.readlines()
+                    print "disregard", data
+                except:
+                    pass
+                time.sleep(0.2)
+                send_data = False
+
+            c = " "
+            #while cont:
+            #    c = sys.stdin.read(1)
+            #    cont = c is not None
+            #    print  c
             l = serr.readline()
             if l != "" and l!="\n":
                 print "##", l
